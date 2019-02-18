ed0788d937c8f6280272f3a4084c3aaa6888e4c1
lizbur10 <liz.burton147@gmail.com>
2019-02-18 16:47:37 -0500

    added event description to db

--- db/migrate/01_create_events.rb
+++ db/migrate/01_create_events.rb
@@ -5,6 +5,7 @@ class CreateEvents < ActiveRecord::Migration[4.2]
       t.string :dates
       t.string :deal_url
       t.string :website_url
+      t.string :details_url
       t.string :description
       t.integer :category_id
       t.integer :venue_id
